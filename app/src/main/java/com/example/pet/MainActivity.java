76062af29f18d470f8365fe89f50ea416cfd855a
Chawuala22 <mrojasgarcia22@gmail.com>
2020-08-25 19:10:10 -0500

    Ready views

--- app/src/main/java/com/example/pet/MainActivity.java
+++ app/src/main/java/com/example/pet/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.pet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton add,dog,cat,vetr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.inscrbirse);
+        dog = findViewById(R.id.perro);
+        cat = findViewById(R.id.gato);
+        vetr = findViewById(R.id.veterinario);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent i = new Intent(MainActivity.this,Add.class);
+                startActivity(i);
+
+            }
+        });
+
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent i = new Intent(MainActivity.this,Cat.class);
+                startActivity(i);
+
+            }
+        });
+
+        dog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent i = new Intent(MainActivity.this,Dog.class);
+                startActivity(i);
+
+            }
+        });
+
+        vetr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent i = new Intent(MainActivity.this,Veterinarian.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
